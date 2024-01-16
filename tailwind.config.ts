d2c701318a2f15616a9d6a65c9e35fe7c106d515
namjun <wrasf175@naver.com>
2024-01-17 00:34:42 +0900

    style : 테일윈드 color extend 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        main : "FFC300",
+        highlight: "03566",
+        background: "F8F8F8",
+        arrow : 'E8E8E8',
+      },
     },
   },
   plugins: [],
