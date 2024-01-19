07c72f60d824eb40bd0b7ab71e864e3c6a2de4df
Yoon Seoan <earthdyoon@gmail.com>
2024-01-19 15:40:15 +0900

    style: 네브바 수정, 메인 페이지 퍼블리싱

--- src/app/_component/NavMenu.tsx
+++ src/app/_component/NavMenu.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 import Image from 'next/image'
 import appLogo from '../../../public/AppMainLogo.png'
 
@@ -6,23 +7,25 @@ type Props = {
   isLogin: boolean
 }
 
-//TODO : isLogin property를 nextAuth 적용시 삭제
+// TODO : isLogin property를 nextAuth 적용시 삭제
 export default function NavMenu({ isLogin }: Props) {
   isLogin = false
   return (
     <header>
-      <nav className='bg-white px-4 lg:px-6 py-2.5 shadow'>
-        <div className='flex flex-wrap justify-between items-center mx-auto max-w-screen-xl'>
-          <a href='/' className='flex items-center'>
+      <nav className='flex justify-center bg-white px-12 py-4 shadow'>
+        <div className='grow flex flex-wrap justify-between items-center lg:mx-52 max-w-screen-2xl'>
+          <a href='/'>
             <Image src={appLogo} width={160} height={26} alt='' />
           </a>
-          <div className='flex items-center lg:order-2'>
-            <button
-              type='button'
-              className='text-white bg-main hover:bg-maindark font-medium rounded-3xl text-sm px-4 py-2 text-center'
-            >
-              {isLogin ? '로그아웃' : '로그인'}
-            </button>
+          <div className='lg:order-2'>
+            <Link href='/login'>
+              <button
+                type='button'
+                className='text-white font-semibold text-xl text-center bg-main hover:bg-maindark rounded-[2rem] px-6 py-2.5'
+              >
+                {isLogin ? '로그아웃' : '로그인'}
+              </button>
+            </Link>
           </div>
         </div>
       </nav>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import Link from 'next/link'
-
 import Image from 'next/image'
 import planetIcon from '../../public/YellowPlanetIcon.png'
 import pinIcon from '../../public/YellowPinIcon.png'
@@ -28,92 +27,120 @@ const videos = [
   { title: '대본 제목', department: '소속 기관' },
 ]
 
+const scripts = [
+  '신데렐라',
+  '아기돼지 삼형제',
+  '백설공주',
+  '헨젤과 그레텔',
+  '공주와 개구리',
+  '대본 이름',
+]
+
 export default function Page() {
   return (
     <main>
-      <div className='flex flex-col items-center h-screen'>
-        <Link href='/newroom'>
-          <button
-            type='button'
-            className='relative bg-white hover:bg-gray-100 rounded-3xl w-96 h-24 mx-6 my-6 shadow'
-          >
-            <div className='flex items-center ml-16'>
-              <p className='text-3xl text-highlight font-extrabold'>
-                동화 속으로 떠나기
-              </p>
-              <PlayIcon className='fill-main size-6 ml-2' />
-            </div>
-            <div className='absolute bottom-3 left-[-3rem]'>
-              <Image
-                src={planetIcon}
-                width={100}
-                height={100}
-                alt=''
-                className='hover:animate-bounce'
-              />
-            </div>
-          </button>
-        </Link>
+      <div className='flex justify-center px-12 md:px-24 lg:px-52'>
+        <div className='grow flex flex-col items-center max-w-screen-xl h-screen'>
+          <Link href='/newroom'>
+            <button
+              type='button'
+              className='relative bg-white hover:bg-gray-100 rounded-[3rem] w-96 h-32 ml-20 my-10 shadow'
+            >
+              <div className='flex items-center ml-[4.5rem]'>
+                <p className='text-3xl text-highlight font-extrabold'>
+                  동화 속으로 떠나기
+                </p>
+                <PlayIcon className='fill-main size-6 ml-2' />
+              </div>
+              <div className='absolute bottom-3 left-[-5rem]'>
+                <Image
+                  src={planetIcon}
+                  width={90}
+                  height={90}
+                  alt=''
+                  className='hover:animate-bounce'
+                />
+              </div>
+            </button>
+          </Link>
 
-        <div className='w-[84rem]'>
-          <div className='bg-white rounded-[2rem] h-60 mb-3'>
-            <div className='flex ml-8 mb-3'>
-              <Image
-                src={pinIcon}
-                width={28}
-                height={30}
-                alt=''
-                className='-scale-x-100 -rotate-12 mr-2'
-              />
-              <p className='text-xl text-highlight font-semibold'>
-                내가 나온 연극
-              </p>
-            </div>
-            <div className='flex justify-around'>
-              {myVideos.map((value, key) => {
-                return (
-                  <div key={key}>
-                    <div className='bg-gray-200 rounded-full w-32 h-32 mb-2'></div>
-                    <div className='text-center'>{value}</div>
-                  </div>
-                )
-              })}
-            </div>
-          </div>
-
-          <div className='flex justify-center h-72 mb-3'>
-            <div className='bg-white rounded-[2rem] w-1/2 mr-1'>
-              <div className='flex ml-8 mb-3'>
+          <div className='w-full'>
+            <div className='bg-white rounded-[2rem] h-[24rem] p-4 mb-4'>
+              <div className='flex px-8 py-6 mb-1'>
                 <Image
                   src={pinIcon}
-                  width={28}
-                  height={30}
+                  width={24}
+                  height={28}
                   alt=''
                   className='-scale-x-100 -rotate-12 mr-2'
                 />
-                <p className='text-xl text-highlight font-semibold'>
-                  다른 사람들의 연극 구경하기
+                <p className='text-2xl text-highlight font-semibold pt-1'>
+                  내가 나온 연극
                 </p>
-                <div>
-                  {videos.map((value, key) => {
-                    return (<div key={key}></div>)
-                  })}
-                </div>
+              </div>
+              <div className='flex justify-around'>
+                {myVideos.map((value, key) => {
+                  return (
+                    <div key={key}>
+                      <div className='bg-gray-200 rounded-full w-52 h-52 mb-2'></div>
+                      <div className='text-center'>{value}</div>
+                    </div>
+                  )
+                })}
               </div>
             </div>
 
-            <div className='bg-white rounded-[2rem] w-1/2 ml-2'>
-              <div className='flex ml-8 mb-3'>
-                <Image
-                  src={pinIcon}
-                  width={28}
-                  height={30}
-                  alt=''
-                  className='-scale-x-100 -rotate-12 mr-2'
-                />
-                <p className='text-xl text-highlight font-semibold'>
-                  동화 대본 구경하기
-                </p>
+            <div className='flex justify-center h-[32rem] mb-4'>
+              <div className='bg-white rounded-[2rem] w-1/2 p-4 mr-2'>
+                <div className='flex px-8 py-4 mb-1'>
+                  <Image
+                    src={pinIcon}
+                    width={24}
+                    height={28}
+                    alt=''
+                    className='-scale-x-100 -rotate-12 mr-2'
+                  />
+                  <p className='text-2xl text-highlight font-semibold pt-1'>
+                    다른 사람들의 연극 구경하기
+                  </p>
+                </div>
+
+                <div className='flex flex-wrap justify-center'>
+                  {videos.map((value, key) => {
+                    return (
+                      <div key={key} className='w-64 mx-3 mb-4'>
+                        <div className=' bg-gray-200 rounded-2xl h-40 mb-2'></div>
+                        <div className='truncate text-center'>{`${value.title} | ${value.department}`}</div>
+                      </div>
+                    )
+                  })}
+                </div>
+              </div>
+
+              <div className='bg-white rounded-[2rem] w-1/2 p-4 ml-2'>
+                <div className='flex px-8 py-4 mb-1'>
+                  <Image
+                    src={pinIcon}
+                    width={24}
+                    height={28}
+                    alt=''
+                    className='-scale-x-100 -rotate-12 mr-2'
+                  />
+                  <p className='text-2xl text-highlight font-semibold pt-1'>
+                    동화 대본 구경하기
+                  </p>
+                </div>
+
+                <div className='flex flex-wrap justify-center'>
+                  {scripts.map((value, key) => {
+                    return (
+                      <div key={key} className='w-64 mx-3 mb-4'>
+                        <div className=' bg-gray-200 rounded-2xl h-40 mb-2'></div>
+                        <div className='truncate text-center'>{value}</div>
+                      </div>
+                    )
+                  })}
+                </div>
               </div>
             </div>
           </div>
