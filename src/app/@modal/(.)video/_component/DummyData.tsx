30bbad465447a20c16213301488f8c4c2cfc0744
Yoon Seoan <earthdyoon@gmail.com>
2024-01-22 16:40:53 +0900

    style: 비디오 플레이어 모달 퍼블리싱, 더미 데이터 컴포넌트 분리, 루트 레이아웃 수정

---
+++ src/app/@modal/(.)video/_component/DummyData.tsx
@@ -0,0 +1,69 @@
+// 영상 가져오기 더미데이터
+
+export const myVideos = [
+  {
+    id: '1',
+    data: {
+      title: '6반 5팀 맹고재리의 연극 1',
+      scriptName: '신데렐라',
+      isPublic: true,
+      urlThumbnail: 'String',
+      department: '망고젤리 유치원',
+      party:
+        '신데렐라 : 김싸피_왕자 : 김싸피_계모 : 김싸피_새언니1 : 김싸피_새언니2 : 김싸피_요정 : 김싸피',
+      createAt: '2024-01-16 11:30',
+    },
+  },
+  {
+    id: '2',
+    data: {
+      title: '6반 5팀 맹고재리의 연극 2',
+      scriptName: '아기 돼지 삼형제',
+      isPublic: false,
+      urlThumbnail: 'String',
+      department: '망고젤리 유치원',
+      party:
+        '첫째 돼지 : 김싸피_둘째 돼지 : 김싸피_셋째 돼지 : 김싸피_늑대 : 김싸피',
+      createAt: '2024-01-16 11:30',
+    },
+  },
+  {
+    id: '3',
+    data: {
+      title: '6반 5팀 맹고재리의 연극 3',
+      scriptName: '백설공주',
+      isPublic: true,
+      urlThumbnail: 'String',
+      department: '망고젤리 유치원',
+      party:
+        '첫째 돼지 : 김싸피_둘째 돼지 : 김싸피_셋째 돼지 : 김싸피_늑대 : 김싸피',
+      createAt: '2024-01-16 11:30',
+    },
+  },
+  {
+    id: '4',
+    data: {
+      title: '6반 5팀 맹고재리의 연극 4',
+      scriptName: '대본 이름',
+      isPublic: true,
+      urlThumbnail: 'String',
+      department: '망고젤리 유치원',
+      party:
+        '첫째 돼지 : 김싸피_둘째 돼지 : 김싸피_셋째 돼지 : 김싸피_늑대 : 김싸피',
+      createAt: '2024-01-16 11:30',
+    },
+  },
+  {
+    id: '5',
+    data: {
+      title: '6반 5팀 맹고재리의 연극 5',
+      scriptName: '대본 이름',
+      isPublic: true,
+      urlThumbnail: 'String',
+      department: '망고젤리 유치원',
+      party:
+        '첫째 돼지 : 김싸피_둘째 돼지 : 김싸피_셋째 돼지 : 김싸피_늑대 : 김싸피',
+      createAt: '2024-01-16 11:30',
+    },
+  },
+]
