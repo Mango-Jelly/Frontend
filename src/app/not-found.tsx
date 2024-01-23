ff18fec17e2c94ac139b6c6884cc2623327d3ebf
Yoon Seoan <earthdyoon@gmail.com>
2024-01-23 13:33:36 +0900

    style: 에러 페이지 퍼블리싱

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,37 @@
-import Link from "next/link";
-import {NextPage} from "next";
+import Link from 'next/link'
+import { NextPage } from 'next'
+import Image from 'next/image'
+import errorImg from '../../public/errorImg.svg'
 
 const NotFound: NextPage = () => {
   return (
-    <div>
-      <div>이 페이지는 존재하지 않습니다. 다른 페이지를 검색해 보세요.</div>
+    <div className='absolute top-0 size-full bg-[#AFAFAF]'>
+      <div className='mt-48 ml-96'>
+        <div className='text-9xl font-black mb-12'>Oops!</div>
+        <div className='text-6xl mb-12'>404 Not Found</div>
+        <div className='text-3xl mb-2'>
+          죄송합니다. 페이지를 찾을 수 없습니다.
+        </div>
+        <div className='text-3xl mb-12'>
+          입력하신 주소가 정확한지 다시 확인해 주세요.
+        </div>
+        <a href='/'>
+          <button className='text-3xl text-white text-center bg-[#E6B559] hover:bg-[#DAA94C] rounded-[2rem] w-56 h-20 m-2 shadow'>
+            메인 페이지로
+          </button>
+        </a>
+      </div>
+
+      <Image
+        src={errorImg}
+        width={640}
+        height={640}
+        alt='에러페이지'
+        className='absolute bottom-0 right-0'
+      />
+      <div></div>
     </div>
   )
 }
 
-export default NotFound;
+export default NotFound
