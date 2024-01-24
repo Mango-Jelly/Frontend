880aa9cd0d809ba1cc3b01cd3013c0a2de205994
namjun <wrasf175@naver.com>
2024-01-24 17:52:48 +0900

    deploy 배포 기본세팅

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
-
-const withVideos = require('next-videos')
-
-module.exports = withVideos();
+module.exports = {
+    reactStrictMode: true,
+    output: 'standalone',
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /.(mov|mp4)$/,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {
+                        publicPath: '/_next',
+                        name: '[name].[ext]',
+                    },
+                },
+            ],
+        })
+        return config;
+    }
+};
