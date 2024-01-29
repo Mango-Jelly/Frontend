2db6e546efab0ef04b6ae9a4286bfd100426bd86
Yoon Seoan <earthdyoon@gmail.com>
2024-01-29 10:08:42 +0900

    style: 변수명, 파일명 변경

--- src/app/tempscript/_component/ControllScript.tsx
+++ src/app/tempscript/_component/ControllScript.tsx
@@ -1,5 +1,5 @@
 import { useState, useRef } from 'react'
-import { scriptInfo } from './Dummy'
+import { scriptInfo } from '../data/Dummy'
 
 const sceneNum = scriptInfo.scene.length
 const dialogNums = scriptInfo.scene.map((value) => {
@@ -8,7 +8,11 @@ const dialogNums = scriptInfo.scene.map((value) => {
 
 export const ControllScript = () => {
   const script = scriptInfo
-  const [curIdx, setCurIdx] = useState({ scene: 0, dialog: 0, idx: 0 })
+  const [curSelection, setCurSelection] = useState({
+    scene: 0,
+    dialog: 0,
+    idx: 0,
+  })
   const refs = useRef<null[] | HTMLDivElement[]>([])
 
   function moveScript() {
@@ -17,25 +21,29 @@ export const ControllScript = () => {
   }
 
   function changeIdx() {
-    if (curIdx.dialog < dialogNums[curIdx.scene] - 1) {
-      setCurIdx((prev) => ({
+    if (curSelection.dialog < dialogNums[curSelection.scene] - 1) {
+      setCurSelection((prev) => ({
         ...prev,
         dialog: prev.dialog + 1,
         idx: prev.idx + 1,
       }))
-    } else if (curIdx.scene < sceneNum - 1) {
-      setCurIdx({ scene: curIdx.scene + 1, dialog: 0, idx: curIdx.idx + 1 })
+    } else if (curSelection.scene < sceneNum - 1) {
+      setCurSelection({
+        scene: curSelection.scene + 1,
+        dialog: 0,
+        idx: curSelection.idx + 1,
+      })
     }
   }
 
   function moveScroll() {
     if (refs.current.length > 0) {
-      refs.current[curIdx.idx]?.scrollIntoView({
+      refs.current[curSelection.idx]?.scrollIntoView({
         behavior: 'smooth',
         block: 'center',
       })
     }
   }
 
-  return { script, curIdx, refs, moveScript }
+  return { script, curIdx: curSelection, refs, moveScript }
 }
