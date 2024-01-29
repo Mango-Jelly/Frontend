7fb40c1dee6419fa8173c2d55d354ac185edddff
Yoon Seoan <earthdyoon@gmail.com>
2024-01-29 15:56:49 +0900

    style: 메인 페이지 디자인 수정

--- src/app/_component/MakeRoomButton.tsx
+++ src/app/_component/MakeRoomButton.tsx
@@ -1,33 +1,25 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import planetIcon from '../../../public/YellowPlanetIcon.png'
-import { PlayIcon } from '@heroicons/react/24/solid'
-
+import Link from 'next/link';
+import Image from 'next/image';
+import planetIcon from '../../../public/YellowPlanetIcon.png';
+import { PlayIcon } from '@heroicons/react/24/solid';
 
 export default function MakeRoomButton({ isLogin }: { isLogin: boolean }) {
-    return (
-        <Link href={isLogin ? '/newroom' : '/login'}>
-            <button
-                type='button'
-                className='relative z-0 bg-white hover:bg-gray-100 rounded-[3rem] w-96 h-32 ml-20 my-10 shadow'
-            >
-                <div className='flex items-center ml-[4.5rem]'>
-                    <p className='text-3xl text-highlight font-extrabold'>
-                        {isLogin ? '동화 속으로 떠나기' : '로그인하고 떠나자!'}
-                    </p>
-                    <PlayIcon className='fill-main size-6 ml-2' />
-                </div>
-                <div className='absolute bottom-3 left-[-5rem]'>
-                    <Image
-                        src={planetIcon}
-                        width={90}
-                        height={90}
-                        alt='방생성 버튼'
-                        className='hover:animate-bounce'
-                    />
-                </div>
-            </button>
-        </Link>
-
-    )
+  return (
+    <Link href={isLogin ? '/newroom' : '/login'}>
+      <button
+        type='button'
+        className='relative z-0 bg-white hover:bg-gray-100 rounded-[4rem] w-auto h-32 ml-20 my-10 shadow group'
+      >
+        <div className='flex items-center ml-20 mr-8'>
+          <p className='text-4xl text-highlight font-extrabold'>
+            {isLogin ? '동화 속으로 떠나기' : '로그인하고 떠나자!'}
+          </p>
+          <PlayIcon className='fill-main size-6 ml-2' />
+        </div>
+        <div className='absolute bottom-3 left-[-4.5rem] group-hover:animate-bounce'>
+          <Image src={planetIcon} width={90} height={90} alt='방 생성 버튼' />
+        </div>
+      </button>
+    </Link>
+  );
 }
--- src/app/_component/MyVideos.tsx
+++ src/app/_component/MyVideos.tsx
@@ -1,38 +1,38 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import pinIcon from '../../../public/YellowPinIcon.png'
-import { myVideos } from './DummyData'
+import Link from 'next/link';
+import Image from 'next/image';
+import pinIcon from '../../../public/YellowPinIcon.png';
+import { myVideos } from '../../data/Dummy';
 
-//TODO : 내가 나온 연극 API 연결
-
+// TODO : 내가 나온 연극 API 연결
 export default function MyVideos({ isLogin }: { isLogin: boolean }) {
-    return (
-        <div className='bg-white rounded-[2rem] h-[24rem] p-4 mb-4'>
-            <div className='flex px-8 py-6 mb-1'>
-                <Image
-                    src={pinIcon}
-                    width={24}
-                    height={28}
-                    alt="내가 나온 연극 목록"
-                    className='-scale-x-100 -rotate-12 mr-2'
-                />
-                <p className='text-2xl text-highlight font-semibold pt-1'>
-                    {isLogin ? "내가 나온 연극" : "이렇게 이용해보세요"}
-                </p>
-            </div>{
-                isLogin && <div className='flex justify-around'>
-                    {myVideos.map((value, key) => {
-                        return (
-                            <div key={key} className='rounded-xl p-2 hover:bg-gray-100'>
-                                <Link href={`/video/${value.videoId}`}>
-                                    <div className='bg-gray-200 rounded-full w-52 h-52 mb-2'></div>
-                                    <div className='text-center'>{value.title}</div>
-                                </Link>
-                            </div>
-                        )
-                    })}
-                </div>
-            }
+  return (
+    <div className='bg-white rounded-[2rem] h-[22rem] p-4 mb-4'>
+      <div className='flex px-8 py-2'>
+        <Image
+          src={pinIcon}
+          width={24}
+          height={28}
+          alt='내가 나온 연극 목록'
+          className='-scale-x-100 -rotate-12 mr-4'
+        />
+        <p className='text-3xl text-highlight font-semibold'>
+          {isLogin ? '내가 나온 연극' : '이렇게 이용해보세요'}
+        </p>
+      </div>
+      {isLogin && (
+        <div className='flex justify-around my-2'>
+          {myVideos.map((value, key) => {
+            return (
+              <div key={key} className='rounded-3xl p-2 hover:bg-gray-100'>
+                <Link href={`/video/${value.videoId}`}>
+                  <div className='bg-gray-200 rounded-full size-52 mb-2'></div>
+                  <div className='text-lg text-center'>{value.title}</div>
+                </Link>
+              </div>
+            );
+          })}
         </div>
-    )
+      )}
+    </div>
+  );
 }
