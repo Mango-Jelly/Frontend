d4a54abb76c542313087bb90624b4db23a00534a
Yoon Seoan <earthdyoon@gmail.com>
2024-02-14 18:09:27 +0900

    feat: 비디오 가져오기 API 연결

--- src/app/@modal/(.)video/_component/Video.tsx
+++ src/app/@modal/(.)video/_component/Video.tsx
@@ -28,7 +28,7 @@ export default async function Video({ id, isLogin }: Props) {
     } else {
       fetchedData = await getVideo(id);
     }
-    videoData = fetchedData.data.videos;
+    videoData = fetchedData.data;
   } catch (error) {
     console.error('퍼블릭 비디오 가져오기 에러', error);
   }
@@ -38,25 +38,25 @@ export default async function Video({ id, isLogin }: Props) {
       <BackButton />
       <div className='flex flex-col justify-center items-center bg-white/80 w-[30%] h-[88%] m-2'>
         <div className='bg-gray-200 rounded-full size-72'></div>
-        <p className='text-3xl font-bold m-4'>{videoData.data.scriptName}</p>
+        <p className='text-3xl font-bold m-4'>{videoData.scriptName}</p>
         <div className='flex justify-center items-center text-xl bg-gray-200 rounded-3xl w-4/5 h-20 m-2'>
-          {videoData.data.department}
+          {videoData.department}
         </div>
         <div className='flex justify-center items-center text-xl bg-gray-200 rounded-3xl w-4/5 h-20 m-2'>
-          {videoData.data.createAt}
+          {videoData.createAt}
         </div>
         <div className='flex flex-col justify-center items-center text-xl bg-gray-200 rounded-3xl w-4/5 h-72 m-2'>
-          {videoData.data.party.split('_').map((value, key) => {
+          {videoData.party.map((value: string, key: number) => {
             return <div key={key}>{`${value}\n`}</div>;
           })}
         </div>
       </div>
 
       <div className='flex flex-col justify-center items-center bg-white/80 w-[62%] h-[88%] m-2 p-12'>
-        <div className='text-5xl mb-12 font-bold'>{videoData.data.title}</div>
+        <div className='text-5xl mb-12 font-bold'>{videoData.title}</div>
         <div className='flex justify-end  w-full mb-8'>
           <ArrowDownTrayIcon className='size-8 mx-2' />
-          {videoData.data.isPublic ? (
+          {videoData.isPublic ? (
             <LockOpenIcon className='size-8 mx-2' />
           ) : (
             <LockClosedIcon className='size-8 mx-2' />
@@ -64,7 +64,7 @@ export default async function Video({ id, isLogin }: Props) {
         </div>
         <div className='bg-gray-200 w-full h-[70%] rounded-3xl'>
           <Suspense fallback={<p>Loading video...</p>}>
-            <VideoViewer />
+            <VideoViewer address={videoData.videoAddress} />
           </Suspense>
         </div>
       </div>
