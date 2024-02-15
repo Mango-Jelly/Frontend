1dc7843c76978b17adc106881cc075139ad3f765
Yoon Seoan <earthdyoon@gmail.com>
2024-02-15 17:38:01 +0900

    feat: 비디오 다운로드 기능 및 모달 애니메이션 수정

--- src/app/@modal/(.)video/[id]/page.tsx
+++ src/app/@modal/(.)video/[id]/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useSearchParams } from 'next/navigation';
+import ModalBackground from '@/app/_component/ModalBackground';
 import Video from '../_component/Video';
 
 type Props = {
@@ -14,9 +15,9 @@ export default function Page({ params: { id } }: Props) {
 
   return (
     <div className='absolute top-0 flex justify-center bg-black/40 size-full z-10'>
-      <div className='relative top-28 bg-background background rounded-2xl w-[110rem] h-[64rem] z-10'>
+      <div className='relative top-28 bg-background background rounded-2xl w-[110rem] h-[64rem] z-10 overflow-hidden'>
+        <ModalBackground />
         <Video id={id} isLogin={isLogin} />
-
       </div>
     </div>
   );
